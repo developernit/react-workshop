acde55905b4934a6d6c1b716aebac3bdbe09a528
Nitish Kafle <47726833+kaflenitish@users.noreply.github.com>
2022-03-01 16:36:22 -0500

    delete this commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
